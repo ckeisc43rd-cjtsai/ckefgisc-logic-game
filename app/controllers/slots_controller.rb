03211311cc83ea8ea5798527be4e95e87084320b
ckeisc43rd-cjtsai <jimtsai9596@gmail.com>
2024-04-09 21:43:02 +0800

    setup ajax for slots

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -6,6 +6,8 @@ class SlotsController < ApplicationController
         @value = (0...8).map { (65 + rand(26)).chr }.join
         respond_to do |format|
             format.js
+
+            format.html
         end
     end
 end
