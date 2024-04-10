d77f1c0c56de147ec92ea139fd745f9e299780d6
ckeisc43rd-cjtsai <jimtsai9596@gmail.com>
2024-04-10 09:34:13 +0800

    finish slots expect rewarding policy

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,8 +1,12 @@
 class SlotsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:update_roulettes]
+
+    $value = "xxx"
     def index
-        @value = (0...3).map { rand(10) }.join
+        $value = "XXX"#@value?@value:"xxx"
+=begin
         respond_to do |format|
-            format.json do
+            format.js do
                 if @value == "777"
                     current_user.balance += 100
                     current_user.save
@@ -19,15 +23,33 @@ class SlotsController < ApplicationController
                 else
                     current_user.balance -= 50
                     current_user.save
-                end            end
+                end
         end
+=end
     end
-    def update_roulettes
-        @value = (0...8).map { (65 + rand(26)).chr }.join
-        respond_to do |format|
-            format.js
 
-            format.html
+
+    def show
+    
+    end
+    
+    def update_roulettes
+        puts current_user.balance
+        $value = (0...3).map { rand(10) }.join
+        if $value == "777"
+            current_user.balance += 100
+            current_user.save
+        else
+            current_user.balance -= 50
+            current_user.save
         end
+        render json: {value: $value, balance: current_user.balance}
+    end
+    def action
+        # Modify the value as needed
+        @valu = "Modified Value"
+        
+        # Render the modified value as text
+        render plain: @valu
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :slots do
-    member do
-      get 'update_roulettes'
-    end
-  end  # Defines the root path route ("/")
-  post 'slots/update_roulettes' => 'slots#update_roulettes', as: :update_roulettes
+  resources :slots
+  post :spin, :to => 'slots#update_roulettes', as: :spin
+  post '/slots/update_roulettes', to: 'slots#update_roulettes'  
+
   resources :menus
   root "menus#index"
 end
