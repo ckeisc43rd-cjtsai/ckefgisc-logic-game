c1cc844050e6b08f5379fe0a748aab27d8765f22
cj.tsai <jimtsai9596@gmail.com>
2024-04-15 15:36:18 +0800

    add form for user to customize their bet in slots

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -5,18 +5,25 @@ class SlotsController < ApplicationController
     def index
        @value = "XXX"
        @user=current_user
+       @slot_bet = @user.slot_bet
     end
     
     def update_roulettes
-        puts current_user.balance
+        number_input = params[:number_input].to_i
+        @user=current_user
+        if number_input!=0
+          @user.slot_bet=number_input
+        end
+        @user.save
+        @slot_bet=@user.slot_bet
         @value = (0...3).map { rand(10) }.join
         if @value == "777"
-            current_user.balance += 100
+            current_user.balance += @slot_bet*2
             current_user.save
         else
-            current_user.balance -= 50
+            current_user.balance -= @slot_bet
             current_user.save
         end
-        render json: {value: @value, balance: current_user.balance}
+        render json: {value: @value, balance: current_user.balance, slot_bet:@slot_bet}
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :slots
+  resources :users
   post :spin, :to => 'slots#update_roulettes', as: :spin
   post '/slots/update_roulettes', to: 'slots#update_roulettes'  
 
